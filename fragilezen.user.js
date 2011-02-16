d9606cde9df538172a64122094b7da3ec6b9831f
Joe Winter <jwinter@gmail.com>
2011-02-16 09:59:55 -0500

    First commit for greasemonkey script for agilezen

---
+++ fragilezen.user.js
@@ -0,0 +1,7 @@
+// ==UserScript==
+// @name          fragilezen
+// @namespace     http://jwinter.org/
+// @description   Show what lane a story is in from the detail page 
+// @include       https://*.agilezen.com/project/*/story/*
+// @include       https://agilezen.com/project/*/story/*
+// ==/UserScript==
