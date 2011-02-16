20d37a28f9e002463e4c680e35dbdee240de99d9
Joe Winter <jwinter@gmail.com>
2011-02-16 10:14:19 -0500

    Think this should work, but try/catch is a little ugly

--- fragilezen.user.js
+++ fragilezen.user.js
@@ -5,3 +5,10 @@
 // @include       https://*.agilezen.com/project/*/story/*
 // @include       https://agilezen.com/project/*/story/*
 // ==/UserScript==
+
+try {
+  var current_lane_a = document.getElementsByClassName('move-story current')[0];
+  current_lane_a.style.fontWeight = "bold";
+  current_lane_a.style.textDecoration = "underline";
+} catch (x) {
+}
